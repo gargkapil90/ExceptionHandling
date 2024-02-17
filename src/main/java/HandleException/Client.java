d3d6df091ce0c663669062906246c7ca403a83ba
Kapil Garg <gargkapil90@gmail.com>
2024-02-17 20:23:17 +0530

    adding exception package

--- src/main/java/HandleException/Client.java
+++ src/main/java/HandleException/Client.java
@@ -1,7 +1,15 @@
 package HandleException;
 
-public class Client {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.List;
 
+public class Client {
+    public static void main(String[] args) throws Exception {
+        //System.out.println(1/0);
+        List<Integer> list = List.of(12, 45, 22, 555,44,39);
+        System.out.println(list.stream().filter(x -> x%2 == 0).reduce(0, (val, ele) -> val + ele));
+
+        Doer doing = new Doer(4);
+        doing.doSomething();
     }
 }
